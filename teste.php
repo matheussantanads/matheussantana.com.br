bc41d139243250d247ea6a579da39ffd66ce12e5
Matheus <matheuss98@outlook.com>
2020-07-14 18:46:34 -0300

    Teste
    Arquivo criado com a finalidade de testar

---
+++ teste.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Olá, sou Matheus Santana. Estudante de Engenharia Eletrônica e Desenvolvedor Full Stack."/>
+    <meta name="author" content="Matheus Santana"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Matheus Santana | Electronic Engineer Student and Full Stack Developer</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="icon" href="./media/image/favicon.png" type="image/png" sizes="512x512">
+</head>
+<body>
+    <span class="red flow-text">Olá, Mundo! HTML</span>
+    <span class="green flow-text"><?php echo "Olá, Mundo! PHP."; ?></span>
+</body>
+</html>
